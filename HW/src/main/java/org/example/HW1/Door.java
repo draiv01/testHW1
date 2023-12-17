707d4f460a83e39425afb5f10d428c8f3eb80e0b
Alexander <119071300+draiv01@users.noreply.github.com>
2023-12-17 22:35:26 +0300

    homework1

---
+++ HW/src/main/java/org/example/HW1/Door.java
@@ -0,0 +1,17 @@
+package HW1;
+
+public class Door {
+
+    /**
+     * Класс для описание двери
+     */
+    private boolean isPrize;
+
+    public Door(boolean isPrize) {
+        this.isPrize = isPrize;
+    }
+
+    public boolean isPrize() {
+        return isPrize;
+    }
+}
